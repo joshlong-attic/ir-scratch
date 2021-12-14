14f55c56d8d92bea88f2230b95c1b5ef13fd5f15
Josh Long <josh@joshlong.com>
2021-12-13 20:13:43 -0800

    build

---
+++ build_aot.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+mvn -DskipTests=true -Pnative  clean package                                                                                                                                                                           1 х │ 6s │ 20:01:56
